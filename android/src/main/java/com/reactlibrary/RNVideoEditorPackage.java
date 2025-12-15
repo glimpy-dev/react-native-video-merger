9cb0d6372ade41b6e94b3219459b36c55738b16a
Eric van der Toorn <eric@evantoor.work>
2025-12-15 22:40:15 +0100

    update android

--- android/src/main/java/com/reactlibrary/RNVideoEditorPackage.java
+++ android/src/main/java/com/reactlibrary/RNVideoEditorPackage.java
@@ -9,16 +9,10 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 public class RNVideoEditorPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNVideoEditorModule(reactContext));
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
     }
 
     @Override
